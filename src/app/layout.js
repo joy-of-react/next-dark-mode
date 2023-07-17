a5933364b4fb45ee2e0501f8d55ce8cd1fa28d86
Joshua Comeau <joshwcomeau@gmail.com>
2023-07-17 12:04:58 -0400

    v2 — basic state-based approach

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,48 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
 
+import {
+  LIGHT_COLORS,
+  DARK_COLORS,
+} from '@/constants';
 import DarkLightToggle from '@/components/DarkLightToggle';
 
 import './styles.css';
 
 function RootLayout({ children }) {
+  const [theme, setTheme] =
+    React.useState('light');
+
+  const COLORS =
+    theme === 'light'
+      ? LIGHT_COLORS
+      : DARK_COLORS;
+
+  function flipTheme() {
+    setTheme(
+      theme === 'light' ? 'dark' : 'light'
+    );
+  }
+
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      data-color-theme={theme}
+      style={{
+        '--color-text': COLORS.text,
+        '--color-background': COLORS.background,
+        '--color-primary': COLORS.primary,
+        '--color-secondary': COLORS.secondary,
+      }}
+    >
       <body>
         <header className="site-header">
           <Link href="">Some Website</Link>
-          <DarkLightToggle />
+          <DarkLightToggle
+            theme={theme}
+            handleClick={flipTheme}
+          />
         </header>
         {children}
       </body>
