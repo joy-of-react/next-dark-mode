a0394c8c6de906a62baa281ffcb3e6a4519dcfbb
Alan Ibarra <alanjibarradev@gmail.com>
2023-07-24 21:38:23 -0700

    modified dark mode

--- src/components/DarkLightToggle/DarkLightToggle.js
+++ src/components/DarkLightToggle/DarkLightToggle.js
@@ -1,73 +1,63 @@
-'use client';
-import React from 'react';
-import { Sun, Moon } from 'react-feather';
-import Cookie from 'js-cookie';
+"use client";
+import React from "react";
+import { Sun, Moon } from "react-feather";
+import Cookie from "js-cookie";
 
-import {
-  LIGHT_COLORS,
-  DARK_COLORS,
-} from '@/constants';
-import VisuallyHidden from '@/components/VisuallyHidden';
-
-import styles from './DarkLightToggle.module.css';
+import { LIGHT_COLORS, DARK_COLORS } from "@/constants";
+import VisuallyHidden from "@/components/VisuallyHidden";
+import { useRouter } from "next/navigation";
+import styles from "./DarkLightToggle.module.css";
 
 function DarkLightToggle({ initialTheme }) {
-  const [theme, setTheme] =
-    React.useState(initialTheme);
-
+  const [theme, setTheme] = React.useState(initialTheme);
+  const router = useRouter();
   function handleClick() {
-    const nextTheme =
-      theme === 'light' ? 'dark' : 'light';
+    const nextTheme = theme === "light" ? "dark" : "light";
 
     setTheme(nextTheme);
 
-    Cookie.set('color-theme', nextTheme, {
+    Cookie.set("color-theme", nextTheme, {
       expires: 1000,
     });
 
-    const COLORS =
-      nextTheme === 'light'
-        ? LIGHT_COLORS
-        : DARK_COLORS;
+    React.startTransition(() => {
+      router.refresh();
+    });
 
-    const root = document.documentElement;
+    // const COLORS =
+    //   nextTheme === 'light'
+    //     ? LIGHT_COLORS
+    //     : DARK_COLORS;
 
-    root.setAttribute(
-      'data-color-theme',
-      nextTheme
-    );
+    // const root = document.documentElement;
 
-    root.style.setProperty(
-      '--color-text',
-      COLORS.text
-    );
-    root.style.setProperty(
-      '--color-background',
-      COLORS.background
-    );
-    root.style.setProperty(
-      '--color-primary',
-      COLORS.primary
-    );
-    root.style.setProperty(
-      '--color-secondary',
-      COLORS.secondary
-    );
+    // root.setAttribute(
+    //   'data-color-theme',
+    //   nextTheme
+    // );
+
+    // root.style.setProperty(
+    //   '--color-text',
+    //   COLORS.text
+    // );
+    // root.style.setProperty(
+    //   '--color-background',
+    //   COLORS.background
+    // );
+    // root.style.setProperty(
+    //   '--color-primary',
+    //   COLORS.primary
+    // );
+    // root.style.setProperty(
+    //   '--color-secondary',
+    //   COLORS.secondary
+    // );
   }
 
   return (
-    <button
-      className={styles.wrapper}
-      onClick={handleClick}
-    >
-      {theme === 'light' ? (
-        <Sun size="1.5rem" />
-      ) : (
-        <Moon size="1.5rem" />
-      )}
-      <VisuallyHidden>
-        Toggle dark / light mode
-      </VisuallyHidden>
+    <button className={styles.wrapper} onClick={handleClick}>
+      {theme === "light" ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+      <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
     </button>
   );
 }
